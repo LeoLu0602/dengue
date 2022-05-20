6f51111cfddb07a904510289bc0b15fa5d058999
Leo <lyuholu@gmail.com>
2022-05-20 23:22:22 +0800

    adjustments
    1. new query
    2. output data.csv

--- fetch_data.py
+++ fetch_data.py
@@ -1,14 +1,17 @@
 import pymssql
+import csv
 
 SERVER = '140.116.214.181:1433'
 USER = 'uclab'
 PWD = 'uclabDB2022'
 DB = 'sinopac_data'
+QUERY = 'SELECT [店家類別], lat, lng FROM [飲食店家]'
 
 conn = pymssql.connect(server=SERVER, user=USER, password=PWD, database=DB)  
 cursor = conn.cursor()  
-cursor.execute('SELECT TOP 30 [店家類別], lat, lng FROM [飲食店家]')
+cursor.execute(QUERY)
 row = cursor.fetchone()  
-while row:  
-    print(row)  
-    row = cursor.fetchone()    
+with open("data.csv", "w", newline='') as csv_file:  # Python 3 version    
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow([i[0] for i in cursor.description]) # write headers
+    csv_writer.writerows(cursor)    
